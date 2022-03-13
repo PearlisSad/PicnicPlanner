ad2ab7deaa8c9c40a2f9fa2456f0702bd2c9d4ab
PearlisSad <44513553+PearlisSad@users.noreply.github.com>
2022-03-13 21:01:30 +0000

    changed main method

--- PicnicItemStoreTest.java
+++ PicnicItemStoreTest.java
@@ -6,33 +6,15 @@ import java.util.Scanner;
 
 public class PicnicItemStoreTest {
 
-    public PicnicItemStoreTest(){
 
-    }
-    public static void main(String[] args)throws AssertionError, IOException{
+   public static void main(String[]arg){
         PicnicItemStore store = new PicnicItemStore();
+
         store.put("a", "abiu");
         store.put("bl", "blackberry");
         store.put("bl", "blackcurrent");
         store.put("a", "apricot");
-
-        //if(!store.map.containsKey("apricot")){
-        //    throw new java.lang.AssertionError();
-        //}
         
-        PicnicItemStore storeNew = new PicnicItemStore("cheeses.txt");
-        try{
-            File file = new File("cheeses.txt");
-            Scanner reader = new Scanner(file);
-            String word = reader.nextLine();
-            if (!storeNew.map.containsKey(word)){
-                throw new java.lang.AssertionError();
-            }
-            reader.close();
-        } catch(FileNotFoundException e){
-            System.err.println("File not found different one" + e);
-        }
-        System.out.println(storeNew);
 
     }
 }
