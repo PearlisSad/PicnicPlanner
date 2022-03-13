d3c043dab4c90d76cb5269982c3e5d6f425e802b
PearlisSad <44513553+PearlisSad@users.noreply.github.com>
2022-03-13 23:16:50 +0000

    failed testQ3_PersonalisedSuggestion

--- PicnicItemStoreTest.java
+++ PicnicItemStoreTest.java
@@ -15,11 +15,11 @@ public class PicnicItemStoreTest {
         store.put("bl", "blackcurrent");
         store.put("a", "apricot");
 
-       DrinkStore ds0 = new DrinkStore();
+       /*DrinkStore ds0 = new DrinkStore();
        ds0.put("a", "Aquafina 0");
        ds0.put("c", "Clos de los Siete Uco Valley 8");
        ds0.put("s", "Salvatore Molettieri Taurasi Vigna Cinque Querce 16");
-       System.out.println(ds0.getRandomItem("a"));
+       System.out.println(ds0.getRandomItem("c"));*/
 
    }
 }
